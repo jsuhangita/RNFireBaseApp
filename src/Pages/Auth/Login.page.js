f059f5935f42da3cd17dedad8aec03d3ed7d698b
jsuhangita <jsuhangita@gmail.com>
2020-08-10 14:34:26 +0700

    Facebook Authentication & user authentication

--- src/Pages/Auth/Login.page.js
+++ src/Pages/Auth/Login.page.js
@@ -1,12 +1,54 @@
 import React ,{ Component } from 'react';
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, Alert} from 'react-native';
 import {SocialIcon, Input, Button} from "react-native-elements";
+import auth from '@react-native-firebase/auth';
+import { LoginManager, AccessToken } from 'react-native-fbsdk';
+
+
 
 
 export default class Login extends Component{
     constructor(props) {
         super(props);
+        this._onFacebookButtonPress = this._onFacebookButtonPress.bind(this);
+        this._onPressLogin = this._onPressLogin.bind(this);
+        this.state={
+            email:'',
+            password:''
+        }
+    }
 
+    async _onFacebookButtonPress() {
+        // Attempt login with permissions
+        const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+
+        if (result.isCancelled) {
+            throw 'User cancelled the login process';
+        }
+
+        // Once signed in, get the users AccesToken
+        const data = await AccessToken.getCurrentAccessToken();
+
+        if (!data) {
+            throw 'Something went wrong obtaining access token';
+        }
+
+        // Create a Firebase credential with the AccessToken
+        const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(facebookCredential);
+    }
+
+    async _onPressLogin(){
+        try {
+            const { email,password } = this.state;
+            const  res = await auth().createUserWithEmailAndPassword(email, password)
+            Alert.alert('Login Success');
+            console.log(res);
+        }catch (e) {
+            Alert.alert('Login Failed')
+        }
     }
 
     render() {
@@ -20,6 +62,7 @@ export default class Login extends Component{
                                 title='Facebook'
                                 button
                                 type='facebook'
+                                onPress={this._onFacebookButtonPress}
                             />
                         </View>
                         <View style={styles.socialButtonContainer}>
@@ -35,15 +78,21 @@ export default class Login extends Component{
                         placeholder='Your email'
                         placeholderTextColor={'grey'}
                         inputContainerStyle={styles.input}
+                        onChangeText={(value)=>{this.setState({email:value})}}
                     />
                     <Input
                         placeholder='Password'
                         placeholderTextColor={'grey'}
                         inputContainerStyle={styles.input}
+                        onChangeText={(value)=>{this.setState({password:value})}}
+                        textContentType={'password'}
+                        autoCompleteType={'password'}
+                        secureTextEntry={true}
                     />
                     <Button
                         raised
                         title="Login"
+                        onPress={this._onPressLogin}
                     />
                 </View>
             </SafeAreaView>
